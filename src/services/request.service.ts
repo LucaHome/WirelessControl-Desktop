86f043bc68ea2e666c9a39800fc781aa4baaab94
Jonas Schubert <guepardoapps@gmail.com>
2019-03-26 16:35:14 +0100

    cleans up request.service

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -1,65 +1,67 @@
 import axios from "axios";
-import { Entity } from "../models/entity";
+import { ApiResponse, Entity } from "../models";
 
 const baseUrl: string = "/index.php/apps/wirelesscontrol/api/v1/";
+const headers: any = { "Content-Type": "application/json" };
 
-export default {
-    get(url: string) {
-        return axios(`${baseUrl}${url}`)
-            .then((response) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    },
-    post<T extends Entity>(url: string, data: T) {
-        delete data['id'];
-        // TODO check contentType => maybe config in headers (contentType: 'application/json')
-        return axios({
-            method: 'post',
-            url: `${baseUrl}${url}`,
-            data: data
+export const get = <K>(url: string): Promise<ApiResponse<K>> => {
+    return axios(`${baseUrl}${url}`)
+        .then((response: any) => {
+            return Promise.resolve(response.data);
         })
-            .then((response) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    },
-    put<T extends Entity>(url: string, data: T) {
-        // TODO check contentType => maybe config in headers (contentType: 'application/json')
-        return axios({
-            method: 'put',
-            url: `${baseUrl}${url}/${data.id}`,
-            data: data
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
+};
+
+export const post = <T extends Entity, K>(url: string, data: T): Promise<ApiResponse<K>> => {
+    delete data["id"];
+    return axios({
+        method: "post",
+        url: `${baseUrl}${url}`,
+        data: data,
+        headers: headers
+    })
+        .then((response) => {
+            return Promise.resolve(response.data);
         })
-            .then((response) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    },
-    delete(url: string, id: number) {
-        return axios({
-            method: 'delete',
-            url: `${baseUrl}${url}/${id}`
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
+};
+
+export const put = <T extends Entity, K>(url: string, data: T): Promise<ApiResponse<K>> => {
+    return axios({
+        method: "put",
+        url: `${baseUrl}${url}/${data.id}`,
+        data: data,
+        headers: headers
+    })
+        .then((response) => {
+            return Promise.resolve(response.data);
         })
-            .then((response) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    }
-}
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
+};
+
+export const destroy = <K>(url: string, id: number): Promise<ApiResponse<K>> => {
+    return axios({
+        method: "delete",
+        url: `${baseUrl}${url}/${id}`
+    })
+        .then((response) => {
+            return Promise.resolve(response.data);
+        })
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
+};
