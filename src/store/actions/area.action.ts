3de2f79ab69fdb121870ecf890a54cc220744e32
Jonas Schubert <guepardoapps@gmail.com>
2019-03-26 16:56:07 +0100

    Update area.action.ts

--- src/store/actions/area.action.ts
+++ src/store/actions/area.action.ts
@@ -1,35 +1,137 @@
 import { Area } from "../../models";
-import { ADD_AREA, DELETE_AREA, LOAD_AREAS, SELECT_AREA, UPDATE_AREA } from "../action-types";
+import {
+    AREA_ADD, AREA_ADD_FAIL, AREA_ADD_ON_SERVER, AREA_ADD_SUCCESSFUL,
+    AREA_DELETE, AREA_DELETE_FAIL, AREA_DELETE_ON_SERVER, AREA_DELETE_SUCCESSFUL,
+    AREA_SELECT, AREA_SELECT_FAIL, AREA_SELECT_SUCCESSFUL,
+    AREA_UPDATE, AREA_UPDATE_FAIL, AREA_UPDATE_ON_SERVER, AREA_UPDATE_SUCCESSFUL,
+    AREAS_LOAD, AREAS_LOAD_FAIL, AREAS_LOAD_SUCCESSFUL,
+} from "../action-types";
 
-export const addArea = (area: Area) => ({
-    payload: {
-        area,
-    },
-    type: ADD_AREA,
-});
-
-export const deleteArea = (area: Area) => ({
-    payload: {
-        area,
-    },
-    type: DELETE_AREA,
-});
-
-export const loadArea = () => ({
+export const areasLoad = () => ({
     payload: {},
-    type: LOAD_AREAS,
+    type: AREAS_LOAD,
 });
 
-export const selectArea = (area: Area) => ({
+export const areasLoadFail = (error: any) => ({
     payload: {
-        area,
+        error,
+        list: undefined,
     },
-    type: SELECT_AREA,
+    type: AREAS_LOAD_FAIL,
 });
 
-export const updateArea = (area: Area) => ({
+export const areasLoadSuccessful = (list: Area[]) => ({
+    payload: {
+        error: undefined,
+        list,
+    },
+    type: AREAS_LOAD_SUCCESSFUL,
+});
+
+export const areaSelect = () => ({
+    payload: {},
+    type: AREA_SELECT,
+});
+
+export const areaSelectFail = (error: any) => ({
+    payload: {
+        area: undefined,
+        error,
+    },
+    type: AREA_SELECT_FAIL,
+});
+
+export const areaSelectSuccessful = (area: Area) => ({
     payload: {
         area,
+        error: undefined,
     },
-    type: UPDATE_AREA,
+    type: AREA_SELECT_SUCCESSFUL,
+});
+
+export const areaAdd = (area: Area) => ({
+    payload: {
+        area,
+        error: undefined,
+    },
+    type: AREA_ADD,
+});
+
+export const areaAddOnServer = () => ({
+    payload: {},
+    type: AREA_ADD_ON_SERVER,
+});
+
+export const areaAddFail = (error: any) => ({
+    payload: {
+        area: undefined,
+        error,
+    },
+    type: AREA_ADD_FAIL,
+});
+
+export const areaAddSuccessful = (area: Area) => ({
+    payload: {
+        area,
+        error: undefined,
+    },
+    type: AREA_ADD_SUCCESSFUL,
+});
+
+export const areaUpdate = (area: Area) => ({
+    payload: {
+        area,
+        error: undefined,
+    },
+    type: AREA_UPDATE,
+});
+
+export const areaUpdateOnServer = () => ({
+    payload: {},
+    type: AREA_UPDATE_ON_SERVER,
+});
+
+export const areaUpdateFail = (error: any) => ({
+    payload: {
+        area: undefined,
+        error,
+    },
+    type: AREA_UPDATE_FAIL,
+});
+
+export const areaUpdateSuccessful = (area: Area) => ({
+    payload: {
+        area,
+        error: undefined,
+    },
+    type: AREA_UPDATE_SUCCESSFUL,
+});
+
+export const areaDelete = (area: Area) => ({
+    payload: {
+        area,
+        error: undefined,
+    },
+    type: AREA_DELETE,
+});
+
+export const areaDeleteOnServer = () => ({
+    payload: {},
+    type: AREA_DELETE_ON_SERVER,
+});
+
+export const areaDeleteFail = (error: any) => ({
+    payload: {
+        area: undefined,
+        error,
+    },
+    type: AREA_DELETE_FAIL,
+});
+
+export const areaDeleteSuccessful = (area: Area) => ({
+    payload: {
+        area,
+        error: undefined,
+    },
+    type: AREA_DELETE_SUCCESSFUL,
 });
