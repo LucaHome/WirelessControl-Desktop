b0dc73c65a4076722b252bf6bac33f6bda3aa690
Jonas Schubert <guepardoapps@gmail.com>
2019-03-27 20:41:17 +0100

    adds model NextCloudCredentials

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 export * from "./api-response";
 export * from "./area";
 export * from "./entity";
+export * from "./next-cloud-credentials";
 export * from "./periodic-task";
 export * from "./wireless-socket";
---
+++ src/models/next-cloud-credentials.d.ts
@@ -0,0 +1,5 @@
+export interface NextCloudCredentials {
+    baseUrl: string;
+    userName: string;
+    passPhrase: string;
+}
