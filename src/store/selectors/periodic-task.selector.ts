40c7b42fa334173d9706710bf3d632853d5013ca
Jonas Schubert <guepardoapps@gmail.com>
2019-03-27 20:49:12 +0100

    fix selectors for selectedEntries

--- src/store/selectors/periodic-task.selector.ts
+++ src/store/selectors/periodic-task.selector.ts
@@ -1,3 +1,3 @@
 export const getPeriodicTaskState = (store: any) => store.periodicTaskReducer;
 export const getPeriodicTaskList = (store: any) => getPeriodicTaskState(store) ? getPeriodicTaskState(store).periodicTasks : [];
-export const getSelectedPeriodicTask = (store: any) => getPeriodicTaskState(store) ? getPeriodicTaskState(store).periodicTaskSelected : [];
+export const getSelectedPeriodicTask = (store: any) => getPeriodicTaskState(store) ? getPeriodicTaskState(store).periodicTaskSelected : undefined;
