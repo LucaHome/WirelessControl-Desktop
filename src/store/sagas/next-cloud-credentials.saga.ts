00e1a6cb81acaf4537695ac8a250009f4c0628e1
Jonas Schubert <guepardoapps@gmail.com>
2019-03-28 12:24:50 +0100

    uses actions in saga, adds missing action in catch-block

--- src/store/sagas/next-cloud-credentials.saga.ts
+++ src/store/sagas/next-cloud-credentials.saga.ts
@@ -1,9 +1,7 @@
 import { put } from "redux-saga/effects";
 import { NextCloudCredentials } from "../../models";
 import { get } from "../../services";
-import {
-    NEXT_CLOUD_CREDENTIALS_LOGIN_FAIL, NEXT_CLOUD_CREDENTIALS_LOGIN_SUCCESSFUL,
-} from "../action-types";
+import { nextCloudCredentialsLoginFail, nextCloudCredentialsLoginSuccessful } from "../actions";
 
 // worker Saga: will be fired on NEXT_CLOUD_CREDENTIALS_LOGIN actions
 export function* login(action) {
@@ -15,26 +13,26 @@ export function* login(action) {
                     // 401 For invalid userName with message: CORS requires basic auth
                     // 401 For invalid passPhrase with message: CORS requires basic auth
                     case 401:
-                        put({ type: NEXT_CLOUD_CREDENTIALS_LOGIN_FAIL, payload: { error: "Invalid Credentials" } });
+                        put(nextCloudCredentialsLoginFail("Invalid Credentials"));
                         break;
                     // 404 For invalid URL
                     // 405 For invalid URL
                     case 404:
                     case 405:
-                        put({ type: NEXT_CLOUD_CREDENTIALS_LOGIN_FAIL, payload: { error: "Invalid URL" } });
+                        put(nextCloudCredentialsLoginFail("Invalid URL"));
                         break;
                     case 200:
-                        put({ type: NEXT_CLOUD_CREDENTIALS_LOGIN_SUCCESSFUL, payload: { nextCloudCredentials } });
+                        put(nextCloudCredentialsLoginSuccessful(nextCloudCredentials));
                         break;
                     default:
-                        put({ type: NEXT_CLOUD_CREDENTIALS_LOGIN_FAIL, payload: { error: `Unknown error: ${response.statusText}` } });
+                        put(nextCloudCredentialsLoginFail(`Unknown error: ${response.statusText}`));
                         break;
                 }
             })
             .catch((error) => {
-                const b = 5;
+                put(nextCloudCredentialsLoginFail(`Unknown error: ${error.message}`));
             });
-    } catch (e) {
-        yield put({ type: NEXT_CLOUD_CREDENTIALS_LOGIN_FAIL, payload: { error: `Unknown error: ${e.message}` } });
+    } catch (error) {
+        yield put(nextCloudCredentialsLoginFail(`Unknown error: ${error.message}`));
     }
 }
