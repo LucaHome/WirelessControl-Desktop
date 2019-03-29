8d5248298139f791c7ea5e3fbcb9362639c0cce2
Jonas Schubert <guepardoapps@gmail.com>
2019-03-29 21:36:58 +0100

    adds dummy routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { MemoryRouter, Route, Switch } from "react-router";
-import Home from "../Home/Home";
+// import Home from "../Home/Home";
 import Loading from "../Loading/Loading";
+import Login from "../Login/Login";
 import { IAppProps } from "./IAppProps";
 
 import "../../../styles/main.scss";
@@ -15,7 +16,13 @@ export default class App extends React.Component<IAppProps, any> {
     public render() {
         return <MemoryRouter>
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={Login} />
+                <Route path="/areas" component={Loading} />
+                <Route path="/areas-edit" component={Loading} />
+                <Route path="/wireless-sockets" component={Loading} />
+                <Route path="/wireless-sockets-edit" component={Loading} />
+                <Route path="/periodic-tasks" component={Loading} />
+                <Route path="/periodic-tasks-edit" component={Loading} />
                 <Route path="/loading" component={Loading} />
             </Switch>
         </MemoryRouter>;
