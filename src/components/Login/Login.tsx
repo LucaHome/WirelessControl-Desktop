499924bd27d8c7cd0a3fdca2e8b5715bd93b2311
Jonas Schubert <guepardoapps@gmail.com>
2019-03-29 21:36:49 +0100

    adds dummy login

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { ILoginProps } from "./ILoginProps";
+
+export default class Login extends React.Component<ILoginProps, any> {
+
+    constructor(props: ILoginProps) {
+        super(props);
+    }
+
+    /* tslint:disable */
+    public render() {
+        return <div>
+            Login
+        </div>;
+    }
+}
