d101f12f11fbe8fa2e3a3b0f41ab37e6a1846616
Jonas Schubert <guepardoapps@gmail.com>
2019-04-03 22:06:06 +0200

    adds context and history to renderer

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,13 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as React from "react";
 import * as ReactDom from "react-dom";
-import { Provider } from "react-redux";
+import { Provider, ReactReduxContext } from "react-redux";
 import App from "../components/App/App";
-import store from "../store";
+import configureStore, { history } from "../store";
+
+const store = configureStore(undefined);
 
 ReactDom.render(
-    <Provider store={store}>
-        <App />
+    <Provider store={store} context={ReactReduxContext}>
+        <App history={history} context={ReactReduxContext} />
     </Provider>,
     document.getElementById("app"),
 );
