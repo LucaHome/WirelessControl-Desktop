528d0379baea098a231194aded716a984fae8bf9
Jonas Schubert <guepardoapps@gmail.com>
2019-04-03 21:23:52 +0200

    uses react-router-redux in nextcloud.saga

--- src/store/sagas/next-cloud-credentials.saga.ts
+++ src/store/sagas/next-cloud-credentials.saga.ts
@@ -1,4 +1,6 @@
+import { goBack, push, replace } from "react-router-redux";
 import { put } from "redux-saga/effects";
+import * as Routes from "../../constants/routes.constants";
 import { NextCloudCredentials } from "../../models";
 import RequestService from "../../services/request.service";
 import StorageService from "../../services/storage.service";
@@ -8,6 +10,7 @@ const subUrl: string = "ping";
 
 // worker Saga: will be fired on NEXT_CLOUD_CREDENTIALS_LOGIN actions
 export function* login(action: NextCloudCredentialsAction) {
+    push(Routes.loading);
     try {
         const nextCloudCredentials: NextCloudCredentials = action.payload.nextCloudCredentials;
         yield RequestService.get(subUrl, nextCloudCredentials)
@@ -17,26 +20,32 @@ export function* login(action: NextCloudCredentialsAction) {
                     // 401 For invalid passPhrase with message: CORS requires basic auth
                     case 401:
                         put(nextCloudCredentialsLoginFail("Invalid Credentials"));
+                        goBack();
                         break;
                     // 404 For invalid URL
                     // 405 For invalid URL
                     case 404:
                     case 405:
                         put(nextCloudCredentialsLoginFail("Invalid URL"));
+                        goBack();
                         break;
                     case 200:
                         StorageService.saveNextCloudCredentials(nextCloudCredentials);
                         put(nextCloudCredentialsLoginSuccessful(nextCloudCredentials));
+                        replace(Routes.areas);
                         break;
                     default:
                         put(nextCloudCredentialsLoginFail(`Unknown error: ${response.statusText}`));
+                        goBack();
                         break;
                 }
             })
             .catch((error) => {
                 put(nextCloudCredentialsLoginFail(`Unknown error: ${error.message}`));
+                goBack();
             });
     } catch (error) {
         yield put(nextCloudCredentialsLoginFail(`Unknown error: ${error.message}`));
+        goBack();
     }
 }
