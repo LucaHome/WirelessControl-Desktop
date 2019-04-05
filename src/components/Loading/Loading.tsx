82928a66fa388efb4e75a74c4b05e6c3d09e0d48
Jonas Schubert <guepardoapps@gmail.com>
2019-04-05 22:02:41 +0200

    adds loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Spinner } from 'reactstrap';
+import { ILoadingProps } from "./ILoadingProps";
+import "./Loading.css";
+
+export default class Loading extends React.Component<ILoadingProps, any> {
+
+    constructor(props: ILoadingProps) {
+        super(props);
+    }
+
+    public render() {
+        return <div className="centered-loading">
+            <Spinner type="grow" color="primary" />
+            Loading...
+        </div>;
+    }
+}
