4638f098e569b24b13e85c3eeb005af03552d21c
Jonas Schubert <guepardoapps@gmail.com>
2019-04-05 15:14:47 +0200

    adds NotFound component

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { INotFoundProps } from "./INotFoundProps";
+import "./NotFound.css";
+
+export default class NotFound extends React.Component<INotFoundProps, any> {
+
+    constructor(props: INotFoundProps) {
+        super(props);
+    }
+
+    public render() {
+        return <div className="NotFound">
+            <h3>Sorry, page not found!</h3>
+        </div>;
+    }
+}
