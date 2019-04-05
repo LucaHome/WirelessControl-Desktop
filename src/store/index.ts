c1a47b657558f86190395f20c1dd791e9a083872
Jonas Schubert <guepardoapps@gmail.com>
2019-04-05 17:18:18 +0200

    cleans up store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,22 @@
-import { routerMiddleware } from "connected-react-router";
-import { createBrowserHistory } from "history";
 import { applyMiddleware, compose, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 import createRootReducer from "./reducers";
 import rootSaga from "./sagas";
 
-export const history = createBrowserHistory();
-
 export default function configureStore(preloadedState?: any) {
     const sagaMiddleware = createSagaMiddleware();
 
     const store = createStore(
-        createRootReducer(history), // root reducer with router state
+        createRootReducer(),
         preloadedState,
         compose(
             applyMiddleware(
-                routerMiddleware(history), // for dispatching history actions
                 sagaMiddleware,
             ),
         ),
     );
 
     sagaMiddleware.run(rootSaga);
-
-    // Hot reloading
-    if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
-        module.hot.accept("./reducers", () => store.replaceReducer(createRootReducer(history)));
-    }
 
     return store;
 }
