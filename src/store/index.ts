e311a5e1d8bdc8adf9dcbbb1c953a0c459a44dbf
Jonas Schubert <guepardoapps@gmail.com>
2019-04-05 22:12:01 +0200

    exporting middleware saga and removing compose

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,16 @@
-import { applyMiddleware, compose, createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 import createRootReducer from "./reducers";
-import rootSaga from "./sagas";
 
-export default function configureStore(preloadedState?: any) {
-    const sagaMiddleware = createSagaMiddleware();
+export const sagaMiddleware = createSagaMiddleware();
 
-    const store = createStore(
-        createRootReducer(),
-        preloadedState,
-        compose(
-            applyMiddleware(
-                sagaMiddleware,
-            ),
+export default function configureStore(preloadedState?: any): any {
+    return {
+        ...createStore(
+            createRootReducer(),
+            preloadedState,
+            applyMiddleware(sagaMiddleware),
         ),
-    );
-
-    sagaMiddleware.run(rootSaga);
-
-    return store;
+        runSaga: sagaMiddleware.run,
+    }
 }
