455dc11d36b6bca42d0426d5b5f2e325065e9f35
Jonas Schubert <guepardoapps@gmail.com>
2019-04-05 16:11:07 +0200

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,22 @@
-import AppBar from "@material-ui/core/AppBar";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Divider from "@material-ui/core/Divider";
-import Drawer from "@material-ui/core/Drawer";
-import Icon from "@material-ui/core/Icon";
-import IconButton from "@material-ui/core/IconButton";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import { withStyles } from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import {
+    AppBar, CssBaseline, Divider, Drawer, Icon, IconButton, List, ListItem, ListItemText, Toolbar, Typography, withStyles,
+} from "@material-ui/core";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import MenuIcon from "@material-ui/icons/Menu";
+
 import classNames from "classnames";
 import { ConnectedRouter, replace } from "connected-react-router";
 import * as React from "react";
 import { Route, Switch } from "react-router";
+import { Link } from "react-router-dom";
 import { put } from "redux-saga/effects";
+
 import * as Routes from "../../constants/routes.constants";
 import { DrawerEntity } from "../../models/drawer-entity";
-import Loading from "../Loading/Loading";
+import Content from "../Content/Content";
 import Login from "../Login/Login";
+import NotFound from "../NotFound/NotFound";
 import { IAppProps } from "./IAppProps";
 
 import "../../../styles/main.scss";
@@ -74,6 +69,12 @@ const styles = (theme: any) => ({
     hide: {
         display: "none",
     },
+    loginButton: {
+        right: 24,
+        position: "absolute",
+        top: 18,
+        color: "white",
+    },
     menuButton: {
         marginLeft: 12,
         marginRight: 36,
@@ -97,13 +98,18 @@ class App extends React.Component<IAppProps, any> {
     };
 
     private readonly drawerList: DrawerEntity[] = [
-        { id: 0, title: "Areas", icon: "map", iconColor: "primary", action: () => { put(replace(Routes.areas)); } },
-        { id: 1, title: "WirelessSockets", icon: "wifi_tethering", iconColor: "primary", action: () => { put(replace(Routes.wirelessSockets)); } },
-        { id: 2, title: "PeriodicTasks", icon: "alarm", iconColor: "primary", action: () => { put(replace(Routes.periodicTasks)); } },
+        { id: 0, title: "Areas", icon: "map", iconColor: "primary", action: () => { console.log("Areas"); put(replace(Routes.areas)); } },
+        { id: 1, title: "WirelessSockets", icon: "wifi_tethering", iconColor: "primary", action: () => { console.log("WirelessSockets"); put(replace(Routes.wirelessSockets)); } },
+        { id: 2, title: "PeriodicTasks", icon: "alarm", iconColor: "primary", action: () => { console.log("PeriodicTasks"); put(replace(Routes.periodicTasks)); } },
     ];
 
     constructor(props: IAppProps) {
         super(props);
+        console.log(props);
+        put(replace(Routes.login));
+        console.log(props);
+        props.history.replace(Routes.login);
+        console.log(props);
     }
 
     public render() {
@@ -128,6 +134,7 @@ class App extends React.Component<IAppProps, any> {
                     </IconButton>
                     <Typography variant="h6" color="inherit" noWrap />
                 </Toolbar>
+                <Link className={this.props.classes.loginButton} to={Routes.login}>Login</Link>
             </AppBar>
             <Drawer
                 variant="permanent"
@@ -151,7 +158,7 @@ class App extends React.Component<IAppProps, any> {
                 <Divider />
                 <List>
                     {this.drawerList.map((entity, _) => (
-                        <ListItem button key={entity.id}>
+                        <ListItem button key={entity.id} onClick={entity.action}>
                             <Icon color={entity.iconColor}>{entity.icon}</Icon>
                             <ListItemText primary={entity.title} />
                         </ListItem>
@@ -161,14 +168,15 @@ class App extends React.Component<IAppProps, any> {
             <main className={this.props.classes.content}>
                 <div className={this.props.classes.toolbar} />
                 <Switch>
-                    <Route exact path={Routes.login} component={Login} />
-                    <Route path={Routes.loading} component={Loading} />
-                    <Route path={Routes.areas} component={Loading} />
-                    <Route path={Routes.areasEdit} component={Loading} />
-                    <Route path={Routes.wirelessSockets} component={Loading} />
-                    <Route path={Routes.wirelessSocketsEdit} component={Loading} />
-                    <Route path={Routes.periodicTasks} component={Loading} />
-                    <Route path={Routes.periodicTasksEdit} component={Loading} />
+                    <Route path={Routes.login} component={Login} exact />
+                    <Route path={Routes.loading} component={Content} />
+                    <Route path={Routes.areas} component={Content} />
+                    <Route path={Routes.areasEdit} component={Content} />
+                    <Route path={Routes.wirelessSockets} component={Content} />
+                    <Route path={Routes.wirelessSocketsEdit} component={Content} />
+                    <Route path={Routes.periodicTasks} component={Content} />
+                    <Route path={Routes.periodicTasksEdit} component={Content} />
+                    <Route component={NotFound} />
                 </Switch>
             </main>
         </div>
