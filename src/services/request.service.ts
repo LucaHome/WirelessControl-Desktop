26f74119ccc5bca158eb648708a3a24d6178a5f4
Jonas Schubert <guepardoapps@gmail.com>
2019-04-05 22:06:45 +0200

    exporting methods instead of default class for requests

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -14,73 +14,127 @@ const createHeader = (nextCloudCredentials: NextCloudCredentials): any => {
     };
 };
 
-export default class RequestService {
-    public static get<K>(url: string, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> {
-        return axios({
-            headers: createHeader(nextCloudCredentials),
-            method: "get",
-            url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}`,
+export const serverGet = async <K>(url: string, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> => {
+    return axios({
+        headers: createHeader(nextCloudCredentials),
+        method: "get",
+        url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}`,
+    })
+        .then((response: any) => {
+            switch (response.status) {
+                // 401 For invalid userName with message: CORS requires basic auth
+                // 401 For invalid passPhrase with message: CORS requires basic auth
+                case 401:
+                    return Promise.resolve("Invalid Credentials");
+                // 404 For invalid URL
+                // 405 For invalid URL
+                case 404:
+                case 405:
+                    return Promise.resolve("Invalid URL");
+                case 200:
+                    return Promise.resolve(response.data);
+                default:
+                    return Promise.resolve(`Unknown error: ${response.statusText}`);
+            }
         })
-            .then((response: any) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    }
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
+}
 
-    public static post<T extends Entity, K>(url: string, data: T, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> {
-        const dataJson = JSON.stringify(data);
-        delete dataJson["id"];
-        return axios({
-            data: dataJson,
-            headers: createHeader(nextCloudCredentials),
-            method: "post",
-            url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}`,
+export const serverPost = async <T extends Entity, K>(url: string, data: T, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> => {
+    const dataJson = JSON.stringify(data);
+    delete dataJson["id"];
+    return axios({
+        data: dataJson,
+        headers: createHeader(nextCloudCredentials),
+        method: "post",
+        url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}`,
+    })
+        .then((response) => {
+            switch (response.status) {
+                // 401 For invalid userName with message: CORS requires basic auth
+                // 401 For invalid passPhrase with message: CORS requires basic auth
+                case 401:
+                    return Promise.resolve("Invalid Credentials");
+                // 404 For invalid URL
+                // 405 For invalid URL
+                case 404:
+                case 405:
+                    return Promise.resolve("Invalid URL");
+                case 200:
+                    return Promise.resolve(response.data);
+                default:
+                    return Promise.resolve(`Unknown error: ${response.statusText}`);
+            }
         })
-            .then((response) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    }
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
+}
 
-    public static put<T extends Entity, K>(url: string, data: T, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> {
-        const dataJson = JSON.stringify(data);
-        return axios({
-            data: dataJson,
-            headers: createHeader(nextCloudCredentials),
-            method: "put",
-            url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}/${data.id}`,
+export const serverPut = async <T extends Entity, K>(url: string, data: T, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> => {
+    const dataJson = JSON.stringify(data);
+    return axios({
+        data: dataJson,
+        headers: createHeader(nextCloudCredentials),
+        method: "put",
+        url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}/${data.id}`,
+    })
+        .then((response) => {
+            switch (response.status) {
+                // 401 For invalid userName with message: CORS requires basic auth
+                // 401 For invalid passPhrase with message: CORS requires basic auth
+                case 401:
+                    return Promise.resolve("Invalid Credentials");
+                // 404 For invalid URL
+                // 405 For invalid URL
+                case 404:
+                case 405:
+                    return Promise.resolve("Invalid URL");
+                case 200:
+                    return Promise.resolve(response.data);
+                default:
+                    return Promise.resolve(`Unknown error: ${response.statusText}`);
+            }
         })
-            .then((response) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    }
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
+}
 
-    public static destroy<K>(url: string, id: number, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> {
-        return axios({
-            headers: createHeader(nextCloudCredentials),
-            method: "delete",
-            url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}/${id}`,
+export const serverDestroy = async <K>(url: string, id: number, nextCloudCredentials: NextCloudCredentials): Promise<ApiResponse<K>> => {
+    return axios({
+        headers: createHeader(nextCloudCredentials),
+        method: "delete",
+        url: `${nextCloudCredentials.baseUrl}${apiUrl}${url}/${id}`,
+    })
+        .then((response) => {
+            switch (response.status) {
+                // 401 For invalid userName with message: CORS requires basic auth
+                // 401 For invalid passPhrase with message: CORS requires basic auth
+                case 401:
+                    return Promise.resolve("Invalid Credentials");
+                // 404 For invalid URL
+                // 405 For invalid URL
+                case 404:
+                case 405:
+                    return Promise.resolve("Invalid URL");
+                case 200:
+                    return Promise.resolve(response.data);
+                default:
+                    return Promise.resolve(`Unknown error: ${response.statusText}`);
+            }
         })
-            .then((response) => {
-                return Promise.resolve(response.data);
-            })
-            .catch((error) => {
-                // tslint:disable
-                console.warn(JSON.stringify(error));
-                return Promise.reject(error);
-            });
-    }
+        .catch((error) => {
+            // tslint:disable
+            console.warn(JSON.stringify(error));
+            return Promise.reject(error);
+        });
 }
