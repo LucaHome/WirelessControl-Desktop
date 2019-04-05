03fcfc8019a2a8795008dbc619846a6da335151d
Jonas Schubert <guepardoapps@gmail.com>
2019-04-05 22:12:33 +0200

    using new store handling and loading credentials on start up

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,12 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import * as React from "react";
 import * as ReactDom from "react-dom";
 import { Provider, ReactReduxContext } from "react-redux";
+
 import App from "../components/App/App";
+import { loadNextCloudCredentialsFromStore } from "../services/storage.service";
 import configureStore from "../store";
+import { nextCloudCredentialsLoginSuccessful } from "../store/actions";
+import rootSaga from "../store/sagas";
+
+const store = configureStore();
+store.runSaga(rootSaga);
+
+const nextCloudCredentials = loadNextCloudCredentialsFromStore();
+if (!!nextCloudCredentials) {
+    store.dispatch(nextCloudCredentialsLoginSuccessful(nextCloudCredentials))
+}
 
 ReactDom.render(
-    <Provider store={configureStore()} context={ReactReduxContext}>
+    <Provider store={store} context={ReactReduxContext}>
         <App context={ReactReduxContext} />
     </Provider>,
     document.getElementById("app"),
