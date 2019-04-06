ba316ea066736cf8831b7198671aa13d0e745abb
Jonas Schubert <guepardoapps@gmail.com>
2019-04-06 11:02:24 +0200

    adds dummy components and routes for areas, wireless sockets and periodic tasks

--- src/constants/routes.constants.ts
+++ src/constants/routes.constants.ts
@@ -1,13 +1,6 @@
 export const login: string = "/";
+export const areas: string = "/areas";
+export const wirelessSockets: string = "/wirelessSockets";
+export const periodicTasks: string = "/periodicTasks";
 export const loading: string = "/loading";
 export const notFound: string = "/not-found";
-export const content: string = "/content";
-
-/*
-export const areas: string = "/areas";
-export const areasEdit: string = "/areas-edit";
-export const wirelessSockets: string = "/wireless-sockets";
-export const wirelessSocketsEdit: string = "/wireless-sockets-edit";
-export const periodicTasks: string = "/periodic-tasks";
-export const periodicTasksEdit: string = "/periodic-tasks-edit";
-*/
