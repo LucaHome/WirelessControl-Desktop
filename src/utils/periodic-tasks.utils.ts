c19b107d19d41ba4c20ff186edcdeb44d5b511f8
Jonas Schubert <guepardoapps@gmail.com>
2019-04-07 16:01:34 +0200

    adds method getDateTimeString for periodicTask

---
+++ src/utils/periodic-tasks.utils.ts
@@ -0,0 +1,6 @@
+import { weekdayArray } from "../constants/periodic-tasks.constants";
+import { PeriodicTask } from "../models";
+
+export const getDateTimeString = (periodicTask: PeriodicTask): string => {
+    return `${weekdayArray[periodicTask.weekday - 1]}, ${("0" + periodicTask.hour).slice(-2)}:${("0" + periodicTask.minute).slice(-2)}`;
+};
