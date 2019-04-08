0733a72e663b792812a7cb25a0ad58acc11a3d42
Jonas Schubert <guepardoapps@gmail.com>
2019-04-08 13:25:29 +0200

    fixes weekday mapping in array

--- src/constants/periodic-tasks.constants.ts
+++ src/constants/periodic-tasks.constants.ts
@@ -1,1 +1,1 @@
-export const weekdayArray: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+export const weekdayArray: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
