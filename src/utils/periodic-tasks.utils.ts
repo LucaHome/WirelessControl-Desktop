0c6bd84ede2d57450466369c354bb88091921901
Jonas Schubert <guepardoapps@gmail.com>
2019-04-08 21:16:26 +0200

    adds method getTimeString for periodic tasks

--- src/utils/periodic-tasks.utils.ts
+++ src/utils/periodic-tasks.utils.ts
@@ -19,6 +19,10 @@ export const getDateTimeString = (periodicTask: PeriodicTask): string => {
     return `${weekdayArray[periodicTask.weekday - 1]}, ${("0" + periodicTask.hour).slice(-2)}:${("0" + periodicTask.minute).slice(-2)}`;
 };
 
+export const getTimeString = (periodicTask: PeriodicTask): string => {
+    return `${("0" + periodicTask.hour).slice(-2)}:${("0" + periodicTask.minute).slice(-2)}`;
+};
+
 export const maxId = (periodicTasks: PeriodicTask[]): number => {
     return Math.max(...periodicTasks.map((periodicTask) => periodicTask.id));
 };
