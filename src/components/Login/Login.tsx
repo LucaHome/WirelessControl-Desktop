51f940b8e136bf9325e4be66ba47283842248392
Jonas Schubert <guepardoapps@gmail.com>
2019-04-09 07:53:00 +0200

    refactors Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -19,34 +19,31 @@ class Login extends React.Component<ILoginProps, any> {
     }
 
     public render() {
-        let urlInput;
-        let urlFormFeedback;
+        let urlInput = <div></div>;
+        let urlFormFeedback = <div></div>;
         if (!this.validateNextCloudUrl()) {
             urlInput = <Input invalid type="url" name="baseUrl" id="baseUrl" placeholder="Enter your server address" onChange={this.handleChange} value={this.state.baseUrl} />;
             urlFormFeedback = <FormFeedback>Invalid Url</FormFeedback>;
         } else {
             urlInput = <Input valid type="url" name="baseUrl" id="baseUrl" placeholder="Enter your server address" onChange={this.handleChange} value={this.state.baseUrl} />;
-            urlFormFeedback = <div></div>;
         }
 
-        let userNameInput;
-        let userNameFormFeedback;
+        let userNameInput = <div></div>;
+        let userNameFormFeedback = <div></div>;
         if (!this.validateUserName()) {
             userNameInput = <Input invalid type="text" name="userName" id="userName" placeholder="Enter your user name" onChange={this.handleChange} value={this.state.userName} />;
             userNameFormFeedback = <FormFeedback>Invalid user name</FormFeedback>;
         } else {
             userNameInput = <Input valid type="text" name="userName" id="userName" placeholder="Enter your user name" onChange={this.handleChange} value={this.state.userName} />;
-            userNameFormFeedback = <div></div>;
         }
 
-        let passPhraseInput;
-        let passPhraseFormFeedback;
+        let passPhraseInput = <div></div>;
+        let passPhraseFormFeedback = <div></div>;
         if (!this.validatePassPhrase()) {
             passPhraseInput = <Input invalid type="password" name="passPhrase" id="passPhrase" placeholder="Enter your password" onChange={this.handleChange} value={this.state.passPhrase} />;
             passPhraseFormFeedback = <FormFeedback>Invalid password</FormFeedback>;
         } else {
             passPhraseInput = <Input valid type="password" name="passPhrase" id="passPhrase" placeholder="Enter your password" onChange={this.handleChange} value={this.state.passPhrase} />;
-            passPhraseFormFeedback = <div></div>;
         }
 
         return (
@@ -74,7 +71,7 @@ class Login extends React.Component<ILoginProps, any> {
         );
     }
 
-    private handleSubmit = (event) => {
+    private handleSubmit = (event: any) => {
         event.preventDefault();
         const nextCloudCredentials: NextCloudCredentials = {
             baseUrl: this.state.baseUrl,
@@ -84,25 +81,25 @@ class Login extends React.Component<ILoginProps, any> {
         this.props.login(nextCloudCredentials);
     }
 
-    private handleChange = (event) => {
+    private handleChange = (event: any) => {
         this.setState({
             [event.target.id]: event.target.value,
         });
     }
 
-    private validateNextCloudUrl = (): boolean => this.state.baseUrl.length > 0;
+    private validateNextCloudUrl = (): boolean => this.state.baseUrl.length > 0 && /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}(\.[a-z]{2,6})?\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(this.state.baseUrl);
     private validatePassPhrase = (): boolean => this.state.passPhrase.length > 0;
     private validateUserName = (): boolean => this.state.userName.length > 0;
     private validateForm = (): boolean => this.validateNextCloudUrl() && this.validatePassPhrase() && this.validateUserName();
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state: any) => {
     return {
         nextCloudCredentials: state.nextCloudCredentials,
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch: any) => {
     return {
         dispatch,
         login: (nextCloudCredentials: NextCloudCredentials) => dispatch(nextCloudCredentialsLogin(nextCloudCredentials)),
