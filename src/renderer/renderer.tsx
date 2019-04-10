d4f397ecc85d01d646de5ee2feace62e35748a73
Jonas Schubert <guepardoapps@gmail.com>
2019-04-10 15:00:24 +0200

    implements possibility to use theme (light & dark)

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -5,13 +5,20 @@ import * as ReactDom from "react-dom";
 import { Provider, ReactReduxContext } from "react-redux";
 
 import MomentUtils from "@date-io/moment";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import { MuiPickersUtilsProvider } from "material-ui-pickers";
 
 import App from "../components/App/App";
-import { loadNextCloudCredentialsFromStore } from "../services/storage.service";
+import { loadAppThemeFromStore, loadNextCloudCredentialsFromStore } from "../services/storage.service";
 import configureStore from "../store";
 import { nextCloudCredentialsLogin } from "../store/actions";
 import rootSaga from "../store/sagas";
+
+const theme = createMuiTheme({
+    palette: {
+        type: loadAppThemeFromStore(),
+    },
+});
 
 const store = configureStore();
 store.runSaga(rootSaga);
@@ -22,10 +29,12 @@ if (!!nextCloudCredentials) {
 }
 
 ReactDom.render(
-    <MuiPickersUtilsProvider utils={MomentUtils}>
-        <Provider store={store} context={ReactReduxContext}>
-            <App context={ReactReduxContext} />
-        </Provider>
-    </MuiPickersUtilsProvider>,
+    <MuiThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+            <Provider store={store} context={ReactReduxContext}>
+                <App context={ReactReduxContext} />
+            </Provider>
+        </MuiPickersUtilsProvider>
+    </MuiThemeProvider >,
     document.getElementById("app"),
 );
