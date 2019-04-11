64090b9b2733db3264c1ff4943f0f96d34d15140
Jonas Schubert <guepardoapps@gmail.com>
2019-04-11 07:44:56 +0200

    removes bootstrap from Loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,12 +1,26 @@
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { withStyles } from "@material-ui/core/styles";
 import * as React from "react";
-import { Spinner } from "reactstrap";
+import { ILoadingProps } from "./ILoadingProps";
+
 import "../../../styles/main.scss";
 
-export default class Loading extends React.Component {
+const styles = (theme: any) => ({
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
+});
+
+class Loading extends React.Component<ILoadingProps, any> {
+    constructor(props: ILoadingProps) {
+        super(props);
+    }
+
     public render() {
         return <div className="wc-window-centered-display">
-            <Spinner type="grow" color="primary" />
-            Loading...
+            <CircularProgress className={this.props.classes.progress} />
         </div>;
     }
 }
+
+export default withStyles(styles, { withTheme: true })(Loading);
